f19ea47df32b54031533b2e823f2e59637d66e8b
MuPeu <murilo.franca09@etec.sp.gov.br>
2025-07-28 16:50:30 -0300

    Primeira experiencia

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#btn").click(function(){
+        //$("p").toggle()
+        /*$(".box").animate({
+            backgroundColor: "green",
+            width: "100px",
+            height: "1000px"
+        })*/
+       $(".box").css({
+            backgroundColor: "green",
+            width: "100px",
+            height: "1000px"
+       })
+    })
+})
